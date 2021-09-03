5b8273c587731ae4e65298621af61dce62ac901f
Azad Yaşar <ceazadyasar@gmail.com>
2021-09-03 10:55:46 +0300

    Implement naive Decoder and DecoderLayer and integrate main Transformer model

--- src/qa/modeling/transformer/__init__.py
+++ src/qa/modeling/transformer/__init__.py
@@ -1,3 +1,5 @@
 from qa.modeling.transformer.multiheadattention import MultiHeadAttentionLayer
 from qa.modeling.transformer.positionwiseff import PositionwiseFeedforwardLayer
 from qa.modeling.transformer.encoder import Encoder
+from qa.modeling.transformer.decoder import Decoder
+from qa.modeling.transformer.transformer import Transformer
